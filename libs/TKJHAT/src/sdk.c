178e536bc121f0a5ecc279a00cb8ff109a8ee9df
MerviAla <154549447+MerviAla@users.noreply.github.com>
2025-10-13 22:00:13 +0300

    Update sdk.c

--- libs/TKJHAT/src/sdk.c
+++ libs/TKJHAT/src/sdk.c
@@ -553,7 +553,20 @@ uint32_t veml6030_read_light() {
     //            Kerro arvo sopivalla kertoimella huomioiden 100 ms integraatioaika ja vahvistus 1/8
     //            käyttäen VEML6030-sovellussuunnitteluasiakirjan sivun 5 tietoja:https://www.vishay.com/docs/84367/designingveml6030.pdf
     //            Lopuksi tallenna arvo muuttujaan luxVal_uncorrected.
-  
+
+    uint8_t txBuffer[1] = {0x04}; //VEML6030_ALS_REG
+    uint8_t rxBuffer[2]; //Vastaanotettava data: LSB, MSB
+    
+    if(i2c_write_blocking(i2c_default, VEML6030_I2C_ADDRESS, txBuffer, 1, true) != PICO_ERROR_GENERIC) {
+        if(i2c_read_blocking(i2c_default, VEML6030_I2C_ADDRESS, rxBuffer, 2, false) != PICO_ERROR_GENERIC) {
+            uint16_t = rawValue = (rxBuffer[1] << 8) | rxBuffer[0];
+
+            float luxVal_uncorrected = rawValue * 0.4608f;
+
+            return (uint32_t)luxVal_uncorrected;
+        }
+    }   
+    
     uint32_t luxVal_uncorrected = 0; 
     if (luxVal_uncorrected>1000){
         // Polynomial is pulled from pg 10 of the datasheet. 
